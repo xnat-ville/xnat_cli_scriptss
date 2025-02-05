f1a701a3d1f63e0d1844dee0b30e496fd96f7fae
Steve Moore <moore.stephen.m@wustl.edu>
2025-02-05 15:49:05 -0600

    Add code to list the projects a user belongs to (without the groups).

--- test_scripts/user_tests.sh
+++ test_scripts/user_tests.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
-run_test() {
- P=/opt/Customer-Support/CNDA/xnat-ville/xnat_cli_scripts/src
- export PYTHONPATH=$P
+# Arguments:
+#              Base Folder
+#              Boiler Plate
+#              User ID
+#              Output file
+list_projects() {
+ export PYTHONPATH="$1/../src"
 
- echo python3 -m xnat_cli_scripts.dicom_metadata $*
-      python3 -m xnat_cli_scripts.dicom_metadata $*
- echo ""
- echo ""
- echo ""
+ echo python3 -m xnat_cli_scripts.users $2 -L --projects -t $3
+      python3 -m xnat_cli_scripts.users $2 -L --projects -t $3 > "$4"
 }
 
 # Arguments:
@@ -39,12 +40,15 @@ remove_groups() {
  BASE_FOLDER=`dirname $0`
  BOILER_PLATE=" -u admin -x http://localhost:8080 -e False "
 
-# list_groups "$BASE_FOLDER" "$BOILER_PLATE" test_user /tmp/user_groups.txt
-# ls -l /tmp/user_groups.txt
+ list_projects "$BASE_FOLDER" "$BOILER_PLATE" test_user /tmp/user_projects.txt
+ ls -l /tmp/user_projects.txt
+
+ list_groups "$BASE_FOLDER" "$BOILER_PLATE" test_user /tmp/user_groups.txt
+ ls -l /tmp/user_groups.txt
 
  remove_groups "$BASE_FOLDER" "$BOILER_PLATE" /tmp/user_groups.txt /tmp/remove_status.txt
  ls -l /tmp/remove_status.txt
 
- remove_groups "$BASE_FOLDER" "$BOILER_PLATE" test_data/user_groups.txt /tmp/remove_user_groups.txt
- ls -l /tmp/remove_user_groups.txt
+# remove_groups "$BASE_FOLDER" "$BOILER_PLATE" test_data/user_groups.txt /tmp/remove_user_groups.txt
+# ls -l /tmp/remove_user_groups.txt
  
