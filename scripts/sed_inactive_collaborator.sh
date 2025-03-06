5b3356da3b8ba474eacb27504844ac3d48aea6b8
Steve Moore <moore.stephen.m@wustl.edu>
2025-03-06 12:40:09 -0600

    Add some tests at the front of sed_inactive_collaborator.sh to ensure the expected input file is present.

--- scripts/sed_inactive_collaborator.sh
+++ scripts/sed_inactive_collaborator.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+
+pre_flight() {
+ for f in       \
+        test_data/inactive_project_groups.txt ; do
+  if [ ! -f $f ] ; then
+   echo Required file is missing: $f
+   echo Script will exit now
+   exit 1
+  fi
+ done
+}
+
+pre_flight
 cat test_data/inactive_project_groups.txt	\
     | sed -e 's/owner$/collaborator/' -e 's/member$/collaborator/'	\
     > test_data/inactive_project_collaborators.txt
